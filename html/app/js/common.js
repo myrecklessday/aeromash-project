448444d5722d043b069064fb2aee16f6e5a98c2d
myrecklessday <sobolhellen@gmail.com>
2017-05-14 22:53:08 +0200

    Tabs were converted to spaces

--- html/app/js/common.js
+++ html/app/js/common.js
@@ -1,56 +1,56 @@
 $(function() {
 
-	$(".slider-wrap").slideDown();
+    $(".slider-wrap").slideDown();
 
-	//Popup animation
+    //Popup animation
 
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
 
-		fixedContentPos: false,
-		fixedBgPos: true,
+        fixedContentPos: false,
+        fixedBgPos: true,
 
-		overflowY: 'auto',
+        overflowY: 'auto',
 
-		closeBtnInside: true,
-		preloader: false,
-		
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
+        closeBtnInside: true,
+        preloader: false,
+        
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
 
-	$("a[href=#callback]").click(function(){
-		$("#callback .formname").val($(this).data("form"));
-	});
+    $("a[href=#callback]").click(function(){
+        $("#callback .formname").val($(this).data("form"));
+    });
 
-	$(".service-item h4").equalHeights();
-	$(".new-item-text").equalHeights();
+    $(".service-item h4").equalHeights();
+    $(".new-item-text").equalHeights();
 
 
-	$(".top-line .sf-menu").superfish({
-		cssArrows: false,
-		hoverClass: "no-class",
-		delay: 300
-	});
+    $(".top-line .sf-menu").superfish({
+        cssArrows: false,
+        hoverClass: "no-class",
+        delay: 300
+    });
 
-	//Owl Carousel
-	var owl = $(".slider");
-	owl.owlCarousel({
-		loop: true,
-		items: 1,
-		itemClass: "slide-wrap",
-		nav: true,
-		navText: "",
-	});
-	
-	$(".next").click(function() {
-		owl.trigger("next.owl.carousel");
-	})
-	
-	$(".prev").click(function() {
-    	owl.trigger("prev.owl.carousel");
-	});
+    //Owl Carousel
+    var owl = $(".slider");
+    owl.owlCarousel({
+        loop: true,
+        items: 1,
+        itemClass: "slide-wrap",
+        nav: true,
+        navText: "",
+    });
+    
+    $(".next").click(function() {
+        owl.trigger("next.owl.carousel");
+    })
+    
+    $(".prev").click(function() {
+        owl.trigger("prev.owl.carousel");
+    });
 
 //Mobile Menu
 
@@ -59,68 +59,68 @@ $(function() {
  $("#my-menu").find("*").attr("style", "");
   $("#my-menu").find("ul").removeClass("sf-menu");
  $("#my-menu").mmenu({
-	 	extensions: [/*'widescreen',*/ 'theme-white', 'effect-menu-slide', 'pagedim-black'],
-	 	navbar: {
-	 		title: "Меню"
-	 	}
-	 });
+        extensions: [/*'widescreen',*/ 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: "Меню"
+        }
+     });
 
 
- 	var api = $("#my-menu").data( "mmenu" );
-	 	api.bind("closed", function(){
-	 		$(".toggle-mnu").removeClass("on");
-	 	});
+    var api = $("#my-menu").data( "mmenu" );
+        api.bind("closed", function(){
+            $(".toggle-mnu").removeClass("on");
+        });
 
 
 
-	 $(".mobile-mnu").click(function() {
-	 	var mmAPI = $("#my-menu").data( "mmenu" );
-	 	mmAPI.open();
-		var thiss = $(this).find(".toggle-mnu");
-	  thiss.toggleClass("on");
-	  $(".main-mnu").slideToggle();
-	  return false;
+     $(".mobile-mnu").click(function() {
+        var mmAPI = $("#my-menu").data( "mmenu" );
+        mmAPI.open();
+        var thiss = $(this).find(".toggle-mnu");
+      thiss.toggleClass("on");
+      $(".main-mnu").slideToggle();
+      return false;
 })
 
 
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+    //SVG Fallback
+    if(!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function() {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$(".callback").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			$(".success").addClass("visible");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-				$(".success").removeClass("visible");
-				$.magnificPopup.close();
-			}, 5000);
-		});
-		return false;
-	});
+    //E-mail Ajax Send
+    //Documentation & Example: https://github.com/agragregra/uniMail
+    $(".callback").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $(".success").addClass("visible");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+                $(".success").removeClass("visible");
+                $.magnificPopup.close();
+            }, 5000);
+        });
+        return false;
+    });
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
 
-	};
+    };
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
